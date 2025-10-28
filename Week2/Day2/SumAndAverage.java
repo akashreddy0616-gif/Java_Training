7274e99cb5cea4c38603dbe14b8258a9cd8d7ce3
akashreddy0616-gif <akashreddy0616@gmail.com>
2025-10-28 15:37:40 -0400

    Day2 SumAndAverage

---
+++ Week2/Day2/SumAndAverage.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class SumAndAverage {
+    public static void main(String[] args) {
+        // Create an ArrayList of numbers
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // Add elements
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        System.out.println("Numbers: " + numbers);
+
+        // Calculate sum
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+
+        // Calculate average
+        double average = (double) sum / numbers.size();
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+    }
+}
