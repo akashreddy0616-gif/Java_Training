7871613b33567febbfa40579a4bb1c2be3faf20c
akashreddy0616-gif <akashreddy0616@gmail.com>
2025-10-27 21:44:55 -0400

    Day2 ReverseString

---
+++ Week1/Day2/ReverseString.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        // Loop through the string from end to start
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversed);
+        scanner.close();
+    }
+}
